cc1f38b0bacc96572d1eafdbfaa71ba82553cf3f
Unknown <55922348+UnknownPlayer78@users.noreply.github.com>
2020-12-31 06:29:35 +0100

    revert

--- lib/ui/pages/evaluations/subjects/view.dart
+++ lib/ui/pages/evaluations/subjects/view.dart
@@ -11,13 +11,10 @@ import 'package:filcnaplo/data/context/app.dart';
 import 'package:filcnaplo/utils/colors.dart';
 import 'package:feather_icons_flutter/feather_icons_flutter.dart';
 import 'package:filcnaplo/ui/pages/evaluations/subjects/average_calc.dart';
-import '../grades/tile.dart';
 
-//ignore: must_be_immutable
 class SubjectView extends StatefulWidget {
   final Subject subject;
   final double classAvg;
-  List<Evaluation> tempEvals = [];
 
   SubjectView(this.subject, this.classAvg);
 
@@ -27,6 +24,7 @@ class SubjectView extends StatefulWidget {
 
 class _SubjectViewState extends State<SubjectView> {
   double studentAvg;
+  List<Evaluation> tempEvals = [];
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +36,7 @@ class _SubjectViewState extends State<SubjectView> {
 
     List<Evaluation> subjectEvals =
         evaluations.where((e) => e.subject.id == widget.subject.id).toList();
-    subjectEvals.addAll(widget.tempEvals);
+    subjectEvals.addAll(tempEvals);
 
     List<GradeTile> evaluationTiles = [];
 
@@ -46,55 +44,68 @@ class _SubjectViewState extends State<SubjectView> {
       if (evaluation.date != null &&
           evaluation.value.value !=
               null) if (evaluation.id.startsWith("temp_")) {
-        evaluationTiles.add(
-            GradeTile(evaluation, deleteCallback: _deleteCallbackFunction));
-      } else
-        evaluationTiles.add(GradeTile(evaluation));
+        evaluationTiles.add(GradeTile(
+          evaluation,
+          padding: EdgeInsets.only(bottom: 6.0),
+          deleteCallback: _deleteCallback,
+        ));
+      } else {
+        evaluationTiles.add(GradeTile(
+          evaluation,
+          padding: EdgeInsets.only(bottom: 6.0),
+        ));
+      }
     });
 
     subjectEvals.forEach((e) {
       studentAvg += e.value.value * (e.value.weight / 100);
     });
 
-    double weight = subjectEvals.map((e) => e.value.weight / 100).reduce((a, b) => a + b);
+    double weight =
+        subjectEvals.map((e) => e.value.weight / 100).reduce((a, b) => a + b);
 
-    if (weight > 0)
-      studentAvg = studentAvg / weight;
+    if (weight > 0) studentAvg = studentAvg / weight;
 
     return Scaffold(
       appBar: AppBar(
         leading: BackButton(),
         title: Text(capital(widget.subject.name)),
+        shadowColor: Colors.transparent,
+        backgroundColor: app.settings.theme.scaffoldBackgroundColor,
         actions: <Widget>[
           studentAvg.round() != 0
-              ? Container(
+              ? Padding(
                   padding: EdgeInsets.fromLTRB(0, 12.0, 8.0, 12.0),
                   child: Container(
+                    width: 55,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(45.0)),
-                      color:
-                          app.theme.evalColors[(studentAvg.round() - 1).clamp(0, 4)],
+                      color: app.theme
+                          .evalColors[(studentAvg.round() - 1).clamp(0, 4)],
                     ),
                     padding: EdgeInsets.all(8.0),
                     child: Text(
                       app.settings.language == "en"
                           ? studentAvg.toStringAsFixed(2)
                           : studentAvg.toStringAsFixed(2).split(".").join(","),
+                      textAlign: TextAlign.center,
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         height: 1.2,
                         color: textColor(
-                          app.theme.evalColors[(studentAvg.round() - 1).clamp(0, 4)],
+                          app.theme
+                              .evalColors[(studentAvg.round() - 1).clamp(0, 4)],
                         ),
                       ),
                     ),
                   ),
-          )
-          : Container(),
+                )
+              : Container(),
           widget.classAvg != null && widget.classAvg.round() != 0
               ? Padding(
                   padding: EdgeInsets.fromLTRB(0, 12.0, 8.0, 12.0),
                   child: Container(
+                    width: 55,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(45.0)),
                       border: Border.all(
@@ -111,6 +122,7 @@ class _SubjectViewState extends State<SubjectView> {
                               .toStringAsFixed(2)
                               .split(".")
                               .join(","),
+                      textAlign: TextAlign.center,
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         height: 1.2,
@@ -128,11 +140,11 @@ class _SubjectViewState extends State<SubjectView> {
             children: <Widget>[
               Container(
                 height: 200.0,
-                padding: EdgeInsets.all(4.0),
-                margin: EdgeInsets.all(4.0),
-                child: SubjectGraph.fromData(
-                  data: subjectEvals,
+                margin: EdgeInsets.fromLTRB(12.0, 14.0, 24.0, 6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.0),
                 ),
+                child: SubjectGraph(subjectEvals),
               ),
               Container(
                 padding:
@@ -163,7 +175,7 @@ class _SubjectViewState extends State<SubjectView> {
           );
           if (tempEval != null) {
             setState(() {
-              widget.tempEvals.add(tempEval);
+              tempEvals.insert(0, tempEval);
             });
           }
         },
@@ -171,9 +183,9 @@ class _SubjectViewState extends State<SubjectView> {
     );
   }
 
-  _deleteCallbackFunction(Evaluation toRemove) {
+  _deleteCallback(Evaluation toRemove) {
     setState(() {
-      widget.tempEvals.removeWhere((e) => e.id == toRemove.id);
+      tempEvals.removeWhere((e) => e.id == toRemove.id);
     });
   }
 }
--- lib/ui/pages/messages/message/tile.dart
+++ lib/ui/pages/messages/message/tile.dart
@@ -1,4 +1,3 @@
-import 'package:filcnaplo/data/context/app.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:filcnaplo/data/models/message.dart';
