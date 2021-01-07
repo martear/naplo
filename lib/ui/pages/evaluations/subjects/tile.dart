6d57e460a0c0e12d688e030a2254307513bfca26
RedyAu <fodor.benedek2001@gmail.com>
2021-01-07 20:07:34 +0100

    subject averages tooltips

--- lib/ui/pages/evaluations/subjects/tile.dart
+++ lib/ui/pages/evaluations/subjects/tile.dart
@@ -1,6 +1,7 @@
 import 'package:feather_icons_flutter/feather_icons_flutter.dart';
 import 'package:filcnaplo/data/context/app.dart';
 import 'package:filcnaplo/data/models/subject.dart';
+import 'package:filcnaplo/generated/i18n.dart';
 import 'package:filcnaplo/helpers/averages.dart';
 import 'package:filcnaplo/ui/pages/evaluations/subjects/view.dart';
 import 'package:filcnaplo/utils/colors.dart';
@@ -49,48 +50,58 @@ class SubjectTile extends StatelessWidget {
                   margin: EdgeInsets.only(right: 8),
                 ),
               classAvg != null && roundSubjAvg(classAvg) != 0
-                  ? Container(
-                      width: 55,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(45.0)),
-                        border: Border.all(
-                          width: 3.0,
-                          color: getAverageColor(classAvg),
+                  ? Tooltip(
+                      message:
+                          capitalize(I18n.of(context).evaluationAverageClass),
+                      child: Container(
+                        width: 55,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                          border: Border.all(
+                            width: 3.0,
+                            color: getAverageColor(classAvg),
+                          ),
                         ),
-                      ),
-                      padding: EdgeInsets.all(5.0),
-                      child: Text(
-                        app.settings.language.split("_")[0] == "en"
-                            ? classAvg.toStringAsFixed(2)
-                            : classAvg.toStringAsFixed(2).split(".").join(","),
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
+                        padding: EdgeInsets.all(5.0),
+                        child: Text(
+                          app.settings.language.split("_")[0] == "en"
+                              ? classAvg.toStringAsFixed(2)
+                              : classAvg
+                                  .toStringAsFixed(2)
+                                  .split(".")
+                                  .join(","),
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
                     )
                   : Container(),
               studentAvg > 0 && studentAvg <= 5.0
-                  ? Container(
-                      width: 55,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(45.0)),
-                        color: getAverageColor(studentAvg),
-                      ),
-                      padding: EdgeInsets.all(8.0),
-                      margin: EdgeInsets.only(left: 8.0),
-                      child: Text(
-                        app.settings.language.split("_")[0] == "en"
-                            ? studentAvg.toStringAsFixed(2)
-                            : studentAvg
-                                .toStringAsFixed(2)
-                                .split(".")
-                                .join(","),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: textColor(
-                            getAverageColor(studentAvg),
-                          ),
+                  ? Tooltip(
+                      message: capitalize(I18n.of(context).evaluationAverage),
+                      child: Container(
+                        width: 55,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                          color: getAverageColor(studentAvg),
                         ),
-                        textAlign: TextAlign.center,
+                        padding: EdgeInsets.all(8.0),
+                        margin: EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          app.settings.language.split("_")[0] == "en"
+                              ? studentAvg.toStringAsFixed(2)
+                              : studentAvg
+                                  .toStringAsFixed(2)
+                                  .split(".")
+                                  .join(","),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: textColor(
+                              getAverageColor(studentAvg),
+                            ),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
                     )
                   : Container(),
