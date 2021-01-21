ea3bd05b9e62902be67e2f37dffc4c2378388010
RedyAu <fodor.benedek2001@gmail.com>
2021-01-21 20:14:22 +0100

    dkt bearer

--- lib/ui/pages/accounts/dkt.dart
+++ lib/ui/pages/accounts/dkt.dart
@@ -1,3 +1,4 @@
+import 'package:filcnaplo/data/context/app.dart';
 import 'package:filcnaplo/data/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -10,12 +11,13 @@ class DKTPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     CookieManager().clearCookies();
-    
+
     return Scaffold(
       body: SafeArea(
         child: WebView(
           javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: "https://dkttanulo.e-kreta.hu/",
+          initialUrl:
+              "https://dkttanulo.e-kreta.hu/sso?accessToken=${app.kretaApi.users[user.id].accessToken}",
         ),
       ),
     );
