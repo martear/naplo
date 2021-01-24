a3c6850677d38dba27c0ed8e2af990a5527a1e10
Unknown <55922348+UnknownPlayer78@users.noreply.github.com>
2021-01-24 15:06:18 +0100

    news fix 2

--- lib/ui/pages/frame.dart
+++ lib/ui/pages/frame.dart
@@ -51,7 +51,7 @@ class _PageFrameState extends State<PageFrame> {
       });
     });
 
-    if (app.user.sync.news.length == 0) {
+    if (app.user.sync.news == null) {
       //Dont display news on first start.
       app.user.sync.news.sync();
     } else {
